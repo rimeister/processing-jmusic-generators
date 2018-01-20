33cbe5250afcd7b0a4512434cc3ed0d36f0f6fdd
Riley Boyd <contact@rileyboyd.com>
2018-01-20 16:17:21 -0500

    Switched to Eclipse IDE for this project, added jMusic to classpath, just has a helloworld example at the moment

--- generator-2/src/Main.java
+++ generator-2/src/Main.java
@@ -1,10 +1,10 @@
+import jm.util.Play;
+
 public class Main {
-
-	public static void Main(String[] args) {
-
-		Nice myNice = new Nice();
-		System.out.println(myNice.val);
-
-	}
 	
+    public static void main(String[] args) {
+
+    	System.out.println("Hello");
+    	
+    }
 }
