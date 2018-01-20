cacf9d4d4fad0469eb0ffb65a9b94e3833fcef57
Riley Boyd <contact@rileyboyd.com>
2018-01-20 16:50:34 -0500

    Added paths to libraries in classpath (including JRE)

--- generator-2/src/Main.java
+++ generator-2/src/Main.java
@@ -1,10 +1,11 @@
+import processing.core.PApplet;
 import jm.util.Play;
 
 public class Main {
-	
-    public static void main(String[] args) {
 
-    	System.out.println("Hello");
-    	
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
     }
+
 }
